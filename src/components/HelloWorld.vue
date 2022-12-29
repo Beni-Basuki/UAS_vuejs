11f795c64bf01d084dc677cd52ec68941822f708
Beni-Basuki <11950115025@students.uin-suska.ac.id>
2022-12-29 23:46:18 +0700

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,54 @@
-<template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
-</template>
-
 <script>
+import "bootstrap/dist/css/bootstrap.css";
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  data() {
+    return {
+      dataMahasiswa: [],
+      nama: "",
+      nim: "",
+    };
+  },
+  methods: {
+    simpanData() {
+      var mahasiswa = {
+        nama: this.nama,
+        nim: this.nim,
+      };
+      this.dataMahasiswa.push(mahasiswa);
+    },
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+<template>
+  <h1>Belajar form dan Vue Event</h1>
+
+  <div class="container">
+    <p class="alert alert-danger">Hello VueJs</p>
+    <p>Nama</p>
+    <input v-model="nama" type="text" class="form-text" />
+    <p>Nim</p>
+    <input v-model="nim" type="text" class="form-text" />
+    <div class="mt-2">
+      <button class="btn btn-info" type="submit" @click.prevent="simpanData">
+        Simpan
+      </button>
+    </div>
+  </div>
+
+  <hr />
+  <table class="table">
+    <thead>
+      <tr>
+        <th>Nama</th>
+        <th>Nim</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="item in dataMahasiswa" :key="item">
+        <td>{{ item.nama }}</td>
+        <td>{{ item.nim }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,41 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <h1 align="center">BIODATA DIRI</h1>
+  <table border="2" cellspacing="0" cellpadding="5" align="center" width="800">
+    <tr align="center" bgcolor="#1fe5d5">
+      <td width="200">DATA DIRI</td>
+      <td width="400">KETERANGAN</td>
+      <td width="200">FOTO</td>
+    </tr>
+    <tr>
+      <td>Nama</td>
+      <td>Beni Basuki</td>
+      <td rowspan="7">
+        <img src="../../public/profil.jpeg" width="200" />
+      </td>
+    </tr>
+    <tr>
+      <td>Tempat/Tanggal Lahir</td>
+      <td>Guntung, 4 Julii 2000</td>
+    </tr>
+    <tr>
+      <td>Alamat</td>
+      <td>Jl. Manyar Sakti</td>
+    </tr>
+    <tr>
+      <td>Agama</td>
+      <td>Islam</td>
+    </tr>
+    <tr>
+      <td>Jenis Kelamin</td>
+      <td>Laki-Laki</td>
+    </tr>
+    <tr>
+      <td>Pekerjaan</td>
+      <td>Mahasiswa</td>
+    </tr>
+    <tr>
+      <td>Hobi</td>
+      <td>Futsal</td>
+    </tr>
+  </table>
 </template>
